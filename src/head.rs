a3c7e0e0d4b44991efb815ee2259a978069781f3
Peter <code@pkratten.de>
2024-01-06 12:52:35 +0100

    Commit comment.

--- src/head.rs
+++ src/head.rs
@@ -9,5 +9,6 @@ pub use crate::handedness::XrHandedness;
 pub use crate::handedness::XrLeft;
 pub use crate::handedness::XrRight;
 
+// TODO: OpenXR allows for multiple views per eye! WebXr probably too.
 #[derive(Component)]
 pub struct XrEye;
